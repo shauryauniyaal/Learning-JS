a239eef5124a807c646c44aa331609ed3c8aeb3d
Shaurya Uniyal <136163561+shauryauniyaal@users.noreply.github.com>
2024-09-08 13:50:48 +0000

    object destructuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -14,13 +14,22 @@ let obj2 = {
 
 let obj3 = {...obj1, ...obj2}
 
-console.log(obj3);
+// console.log(obj3);
 
-console.log(Object.keys(obj3));
+// console.log(Object.keys(obj3));
 
-console.log(Object.values(obj3));
+// console.log(Object.values(obj3));
 
-console.log(Object.entries(obj3));
+// console.log(Object.entries(obj3));
 
-console.log(obj3.hasOwnProperty('1'));
+// console.log(obj3.hasOwnProperty('1'));
 
+let course = {
+    courseName : "JavaScript Basics",
+    coursePrice : 899,
+    courseInstructor : "Xyz" 
+}
+
+let {courseInstructor = instructor} = course // object destructuring
+
+console.log(courseInstructor);
