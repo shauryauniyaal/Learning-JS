8ff6482d26e8134d0c9ae5c32000ad27447e340d
Shaurya Uniyal <136163561+shauryauniyaal@users.noreply.github.com>
2024-10-15 16:03:46 +0000

    for loop

---
+++ 04_control_flow/nullish_&_terniary.js
@@ -0,0 +1,18 @@
+// Nullish Coalescing Operator (??) : Used with null and undefined
+
+let val1;
+
+// val1 = null ?? 14
+val1 = undefined ?? 14
+
+console.log(val1);
+
+
+// Terniary Operator (?) : Kind of a shortcut for if else
+
+// condition ? true : false
+
+let value = 40
+
+value > 40 ? console.log("Value greater than 40") : console.log("Value less than or equal to 40");
+
---
+++ 05_loops/one.js
@@ -0,0 +1,17 @@
+// for 
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+
+    // console.log(element);   
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        console.log();
+        
+        
+    }
+    
+}
